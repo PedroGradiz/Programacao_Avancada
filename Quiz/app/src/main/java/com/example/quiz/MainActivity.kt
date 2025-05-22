e6f72232e28836060ae605fecdca855488f6b0ac
Pedro Gradiz <132215340+PedroGradiz@users.noreply.github.com>
2025-05-22 12:04:42 +0100

    Quiz_Ataulizaçao

--- Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
@@ -7,6 +7,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private val questions=arrayOf("1. Qual é a principal vantagem do Kotlin em relação ao Java para desenvolvimento Android?",
+        "2.Qual palavra-chave é usada para declarar uma variável imutável em Kotlin?",
+        "4.Como se declara uma função em Kotlin?")
+
+    private val questions = arrayOf(
+        arrayOf("A) É mais lento que o Java", "B) Não é compatível com bibliotecas Java", "C) Sintaxe mais concisa e segura", "D) Requer hardware específico"),
+        arrayOf("A) var", "B) const", "C) final", "D) val"),
+        arrayOf("A) def minhaFuncao() {}", "B) fun minhaFuncao() {}", "C) function minhaFuncao() {}", "D) void minhaFuncao() {}")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
