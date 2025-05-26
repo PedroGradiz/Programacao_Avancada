612835f0f54a5eaed242fe0793a411b2ae85a534
Pedro Gradiz <132215340+PedroGradiz@users.noreply.github.com>
2025-05-26 15:25:49 +0100

    Quiz_Atualizaçao
    Atualizaçao do quiz nas tabelas de start e da tela main

--- Quiz/app/src/main/java/com/example/quiz/TelaStart.kt
+++ Quiz/app/src/main/java/com/example/quiz/TelaStart.kt
@@ -1,20 +1,19 @@
 package com.example.quiz
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TelaStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tela_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
